b8e57c1c9825e7285853dd4be7678c6c2c410aed
michaelcyril <michaelcyril71@gmail.com>
2024-02-10 00:22:27 -0800

    initial

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,14 @@
+
+class AppConstants {
+  // static const String appName = 'NAME';
+  // static const double appVersion = x.x;
+
+  // Shared Key
+  // static const String theme = 'theme';
+  // static const String userEmail = 'userEmail';
+
+  // static List<LanguageModel> languages = [
+  //   LanguageModel(imageUrl: Images.unitedKingdom, languageName: 'English', countryCode: 'US', languageCode: 'en'),
+  //   LanguageModel(imageUrl: Images.arabic, languageName: 'Arabic', countryCode: 'SA', languageCode: 'ar'),
+  // ];
+}
---
+++ lib/constants/images.dart
@@ -0,0 +1,12 @@
+class Images {
+  // for Icon
+  // static const String search = 'assets/icon/search.png';
+  // static const String doneWithFullBackground = 'assets/icon/done_with_full_background.png';
+  // static const String location = 'assets/icon/location.png';
+
+  // for Image
+  // static const String arabic = 'assets/image/arabic.png';
+  // static const String unitedKingdom = 'assets/image/united_kindom.png';
+  // static const String done = 'assets/icon/done.png';
+
+}
